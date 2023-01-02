bed0e5c61deff498abcbf202da29623acb20a4b9
Pagon Game Dev <pagongamedev@gmail.com>
2023-01-02 15:11:37 +0700

    update route lint

--- src/frontend/global/route/index.tsx
+++ src/frontend/global/route/index.tsx
@@ -1,21 +1,21 @@
 import { BrowserRouter, Navigate, Outlet, Route, Routes } from 'react-router-dom';
 import { helperI18Next } from 'universal-helper';
 
-import p000Login from '../../domain/p000-login';
-import p001Dashboard from '../../domain/p001-dashboard';
-import p002ThreeJS from '../../domain/p002-threejs';
-import p003ImpactJS from '../../domain/p003-impactjs';
-import p004Menu from '../../domain/p004-menu';
+import P000Login from '../../domain/p000-login';
+import P001Dashboard from '../../domain/p001-dashboard';
+import P002ThreeJS from '../../domain/p002-threejs';
+import P003ImpactJS from '../../domain/p003-impactjs';
+import P004Menu from '../../domain/p004-menu';
 import RoutePrivate from '../component/atoms/route-private';
 import TemplateMobile from '../component/templates/template-mobile';
 import { useStoreGlobalPersist } from '../store/persist';
 
 const i18nList: helperI18Next.TypeI18NDomain[] = [
-  p000Login.I18N,
-  p001Dashboard.I18N,
-  p002ThreeJS.I18N,
-  p003ImpactJS.I18N,
-  p004Menu.I18N,
+  P000Login.I18N,
+  P001Dashboard.I18N,
+  P002ThreeJS.I18N,
+  P003ImpactJS.I18N,
+  P004Menu.I18N,
 ];
 
 const JSX = (props: any) => {
@@ -26,7 +26,7 @@ const JSX = (props: any) => {
       <Routes>
         <Route
           index
-          element={userData ? <Navigate replace to="/user" /> : <p000Login.JSX />}
+          element={userData ? <Navigate replace to="/user" /> : <P000Login.JSX />}
         />
         {/* <Route path="/" element={<p000Login.JSX />} /> */}
         <Route
@@ -40,10 +40,10 @@ const JSX = (props: any) => {
           }
         >
           <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<p001Dashboard.JSX />} />
-          <Route path="threejs" element={<p002ThreeJS.JSX />} />
-          <Route path="impactjs" element={<p003ImpactJS.JSX />} />
-          <Route path="menu" element={<p004Menu.JSX />} />
+          <Route path="dashboard" element={<P001Dashboard.JSX />} />
+          <Route path="threejs" element={<P002ThreeJS.JSX />} />
+          <Route path="impactjs" element={<P003ImpactJS.JSX />} />
+          <Route path="menu" element={<P004Menu.JSX />} />
         </Route>
         <Route path="*" element={<div>URLs Not Fount</div>} />
       </Routes>
